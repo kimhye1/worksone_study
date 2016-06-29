d382c5db01a886400b6b0c01a39a009a790ced03
SooHanJang <soohan.jang@worksmobile.com>
2016-06-29 14:17:06 +0900

    ##  온도 변환 기능 구현 * 온도 변환 기능을 구현했습니다. 화씨를 도씨로 변경하여 표시합니다.

---
+++ WorksOneStudy/WMSChangeTempFormat.h
@@ -0,0 +1,13 @@
+//
+//  WMSChangeTempFormat.h
+//  WorksOneStudy
+//
+//  Created by Naver on 2016. 6. 29..
+//  Copyright © 2016년 worksmobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WMSChangeTempFormat : NSObject
+
+@end
---
+++ WorksOneStudy/WMSChangeTempFormat.m
@@ -0,0 +1,13 @@
+//
+//  WMSChangeTempFormat.m
+//  WorksOneStudy
+//
+//  Created by Naver on 2016. 6. 29..
+//  Copyright © 2016년 worksmobile. All rights reserved.
+//
+
+#import "WMSChangeTempFormat.h"
+
+@implementation WMSChangeTempFormat
+
+@end
